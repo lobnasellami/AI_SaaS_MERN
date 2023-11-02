49b71da55450934aafc4a5c8ee9f7edf2de53d60
LobnaSellami <lobnasellami7@gmail.com>
2023-11-02 13:00:02 +0100

    user authentification JWT and cookies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,14 @@ import express from "express"
 import {config} from"dotenv"
 import morgan from "morgan" 
 import appRouter from "./routes/index.js"
+import cookieParser from "cookie-parser"
 config()
 const app=express()
-
+//middlewares
 app.use(express.json())
+//
+app.use(cookieParser(process.env.COOKIE_SECRET))
+
 // For log information , we remove it in the production 
 app.use(morgan('dev'))
 
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,11 +1,13 @@
 import User from "../models/User.js";
 import {Request,Response,NextFunction }from "express";
 import {hash,compare} from "bcrypt"
+import {createToken} from "../utils/token-manager.js"
 import { ChatCompletionResponseMessageRoleEnum } from "openai";
+import { COOKIE_NAME } from "../utils/contants.js";
 export const getAllUsers=async(req:Request,res:Response,next:NextFunction)=>{
     try{
         const users =await User.find()
-        return res.status(200).json({message :"user are fetched", users:users})
+        return res.status(200).json({message :"users are fetched", users:users})
     }catch(error){
         console.log(error)
         return (res.status(500).json({message:"ERROR",error:error}))
@@ -26,7 +28,20 @@ export  const userSignUp = async(req:Request,res:Response,next:NextFunction)=>{
         const  hashedPassword=await hash(password,10)
         const user = new User({name,email,password:hashedPassword})
         await user.save()
-        return res.status(201).json({message:"OK USer ADDED ",id: user._id.toString()})
+        // create token and store cookie 
+        res.clearCookie(COOKIE_NAME,{domain : "localhost",
+        httpOnly:true,
+        signed:true,
+        path:"/"
+         })
+        const token = createToken(user._id.toString(),user.email.toString(),"7d")
+        // send token to format of cookies we user the cookie-parser package it send to data from backend to frontend
+        const expires =new Date();
+        expires.setDate(expires.getDate()+7)
+        res.cookie(COOKIE_NAME,token,{path: "/", domain : "localhost",expires:expires,httpOnly:true,signed:true})
+
+
+        return res.status(201).json({message:"OK User ADDED ",id: user._id.toString()})
     }catch(error){
         return res.status(500).json({mesage:"ERROR",cause:error.message})
     }
@@ -42,8 +57,19 @@ export  const userLogin = async(req:Request,res:Response,next:NextFunction)=>{
         // compare the password with actual string , we don't unhash then compare the strings
         const isPasswordCorrect =await compare(password,user.password)
         if(!isPasswordCorrect){
-            return res.status(303).send({message:"Wrong Password"})
+            return res.status(403).send({message:"Wrong Password"})
         }
+        // delete the coookie 
+        res.clearCookie(COOKIE_NAME,{domain : "localhost",
+            httpOnly:true,
+            signed:true,
+            path:"/"
+        })
+        const token = createToken(user._id.toString(),user.email.toString(),"7d")
+        // send token to format of cookies we user the cookie-parser package it send to data from backend to frontend
+        const expires =new Date();
+        expires.setDate(expires.getDate()+7)
+        res.cookie(COOKIE_NAME,token,{path: "/", domain : "localhost",expires:expires,httpOnly:true,signed:true})
         return res.status(200).json({message:"Succesfull Login", id: user._id.toString()})
      
     }catch(error){
